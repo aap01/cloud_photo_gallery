b5a27df1e50dba4499c79a989c6b2a8b5a4ea466
aap01 <aap.pavel@gmail.com>
2022-06-14 23:04:45 +0600

    feat: create build_constants.dart

---
+++ lib/core/constant/build_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+
+class BuildConstants {
+  BuildConstants._();
+  static const debug = kDebugMode;
+  static const release = kReleaseMode;
+  static const profile = kProfileMode;
+}
