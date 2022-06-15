f1f372ab66a83dc9290e1f57372ade0dd1e93720
aap01 <aap.pavel@gmail.com>
2022-06-15 17:32:02 +0600

    chore: added ServerFaulure to failure.dart

--- lib/core/failure/failure.dart
+++ lib/core/failure/failure.dart
@@ -1,3 +1,15 @@
+import 'package:cloud_photo_gallery/core/constant/message_constants.dart';
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {}
+
+class ServerFailure extends Failure {
+  final String message;
+
+  ServerFailure({
+    this.message = MessageConstants.defaultServerFailureMessage,
+  });
+
+  @override
+  List<Object?> get props => [message];
+}
