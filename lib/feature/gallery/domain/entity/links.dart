ed7bad7174f8b19c97e45bec638e874898bf12ff
aap01 <aap.pavel@gmail.com>
2022-06-15 08:17:47 +0600

    feat: create links.dart domain entity

---
+++ lib/feature/gallery/domain/entity/links.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Links {
+  final String self;
+  final String html;
+  final String download;
+  final String downloadLocation;
+  Links({
+    required this.self,
+    required this.html,
+    required this.download,
+    required this.downloadLocation,
+  });
+}
