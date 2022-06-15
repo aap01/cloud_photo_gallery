b19e335f92093b51404b0063ba0ba115be59988c
aap01 <aap.pavel@gmail.com>
2022-06-15 08:17:16 +0600

    feat: create urls.dart domain entity

---
+++ lib/feature/gallery/domain/entity/urls.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Urls {
+  final String raw;
+  final String full;
+  final String regular;
+  final String small;
+  final String thumb;
+  final String smallS3;
+  Urls({
+    required this.raw,
+    required this.full,
+    required this.regular,
+    required this.small,
+    required this.thumb,
+    required this.smallS3,
+  });
+}
