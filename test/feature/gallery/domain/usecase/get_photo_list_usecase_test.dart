acf9d7d774e1754e78827b787884e424487d796d
aap01 <aap.pavel@gmail.com>
2022-06-15 17:38:22 +0600

    test: create get_photo_list_usecase_test.dart

---
+++ test/feature/gallery/domain/usecase/get_photo_list_usecase_test.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_photo_gallery/core/failure/failure.dart';
+import 'package:cloud_photo_gallery/core/usecase/params/page_listing_params.dart';
+import 'package:cloud_photo_gallery/feature/gallery/domain/repository/photo_repository.dart';
+import 'package:cloud_photo_gallery/feature/gallery/domain/usecase/get_photo_list_usecase.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'get_photo_list_usecase_test.mocks.dart';
+
+@GenerateMocks([PhotoRepository])
+void main() {
+  final mockPhotoRepository = MockPhotoRepository();
+  final usecase = GetPhotoListUsecase(
+    photoRepository: mockPhotoRepository,
+  );
+  int perPage = 10;
+  int page = 1;
+  final pageListingParams = PageListingParams(
+    perPage: perPage,
+    page: page,
+  );
+  final serverFalure = ServerFailure();
+
+  test(
+    'returns value of Photorepository.getPhotoList method',
+    () async {
+      when(mockPhotoRepository.getPhotos(
+        perPage: perPage,
+        page: page,
+      )).thenAnswer((_) async => Left(serverFalure));
+
+      final result = await usecase(param: pageListingParams);
+
+      verify(
+        mockPhotoRepository.getPhotos(
+          perPage: pageListingParams.perPage,
+          page: pageListingParams.page,
+        ),
+      );
+      expect(result, Left(serverFalure));
+      verifyNoMoreInteractions(mockPhotoRepository);
+    },
+  );
+}
