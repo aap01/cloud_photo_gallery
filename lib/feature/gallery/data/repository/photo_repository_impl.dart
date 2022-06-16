d7bb6c816cec198f2dac16807c3f8b6a7bd5214c
aap01 <aap.pavel@gmail.com>
2022-06-16 20:50:42 +0600

    chore: update photo_repository_impl.dart

--- lib/feature/gallery/data/repository/photo_repository_impl.dart
+++ lib/feature/gallery/data/repository/photo_repository_impl.dart
@@ -1,10 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:cloud_photo_gallery/core/constant/build_constants.dart';
 import 'package:cloud_photo_gallery/core/exception/server_exception.dart';
 import 'package:cloud_photo_gallery/core/failure/failure.dart';
 import 'package:cloud_photo_gallery/core/failure/get_photo_list_failure.dart';
 import 'package:cloud_photo_gallery/feature/gallery/data/data_source/remote/photo_remote_data_source.dart';
-import 'package:cloud_photo_gallery/feature/gallery/data/model/photo_model.dart';
+import 'package:cloud_photo_gallery/feature/gallery/data/model/photo_list_model.dart';
 import 'package:cloud_photo_gallery/feature/gallery/domain/entity/photo.dart';
 import 'package:cloud_photo_gallery/feature/gallery/domain/repository/photo_repository.dart';
 import 'package:dartz/dartz.dart';
@@ -19,7 +18,7 @@ class PhotoRepositoryImpl implements PhotoRepository {
   }
 
   @override
-  Future<Either<Failure, List<Photo>>> getPhotos({
+  Future<Either<GetPhotoListFailure, List<Photo>>> getPhotos({
     required int perPage,
     required int page,
   }) async {
@@ -39,14 +38,9 @@ class PhotoRepositoryImpl implements PhotoRepository {
           );
         },
         success: (value) {
-          final photoList = (value as List).map((e) {
-            final model = PhotoModel.fromJson(e as Map<String, dynamic>);
-            if (BuildConstants.debug) {
-              print(model.toJson());
-            }
-            return model;
-          }).toList();
-          return Right(photoList);
+          final photoListModel =
+              PhotoListModel.fromJson({'photos': value});
+          return Right(photoListModel.photos);
         },
       );
     } on ServerException catch (e) {
