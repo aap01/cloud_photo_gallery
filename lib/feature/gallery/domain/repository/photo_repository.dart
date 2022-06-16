dcd7a14948207293931c0f27e06d6ca8849b6593
aap01 <aap.pavel@gmail.com>
2022-06-16 20:50:52 +0600

    chore: update photo_repository.dart

--- lib/feature/gallery/domain/repository/photo_repository.dart
+++ lib/feature/gallery/domain/repository/photo_repository.dart
@@ -1,9 +1,9 @@
-import 'package:cloud_photo_gallery/core/failure/failure.dart';
+import 'package:cloud_photo_gallery/core/failure/get_photo_list_failure.dart';
 import 'package:cloud_photo_gallery/feature/gallery/domain/entity/photo.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class PhotoRepository {
-  Future<Either<Failure, List<Photo>>> getPhotos({
+  Future<Either<GetPhotoListFailure, List<Photo>>> getPhotos({
     required int perPage,
     required int page,
   });
