0398a7ed386f0325c6d68d1aa969399cb3283128
aap01 <aap.pavel@gmail.com>
2022-06-16 20:48:47 +0600

    fix(domain)!: usecase is now Generic Typed

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -2,6 +2,6 @@ import 'package:dartz/dartz.dart';
 
 import '../failure/failure.dart';
 
-abstract class Usecase<Param, R> {
-  Future<Either<Failure, R>> call({required Param param});
+abstract class Usecase<Param, R, T extends Failure> {
+  Future<Either<T, R>> call({required Param param});
 }
