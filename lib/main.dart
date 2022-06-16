76499b0a62fbaa6d82f8f2afc2febe7aa75ab742
aap01 <aap.pavel@gmail.com>
2022-06-16 11:27:05 +0600

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_photo_gallery/app.dart';
+import 'package:cloud_photo_gallery/core/di/service_locator.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await ServiceLocator.inject();
   runApp(const MyApp());
 }
