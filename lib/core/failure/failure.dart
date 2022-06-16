5f5dd12c339294ef38057ea15f6846af4cbe4c12
aap01 <aap.pavel@gmail.com>
2022-06-16 19:39:29 +0600

    update failure.dart

--- lib/core/failure/failure.dart
+++ lib/core/failure/failure.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_photo_gallery/core/constant/message_constants.dart';
-import 'package:equatable/equatable.dart';
 
-abstract class Failure extends Equatable {}
+abstract class Failure {}
 
 class ServerFailure extends Failure {
   final String message;
@@ -9,7 +8,8 @@ class ServerFailure extends Failure {
   ServerFailure({
     this.message = MessageConstants.defaultServerFailureMessage,
   });
-
-  @override
-  List<Object?> get props => [message];
 }
+
+class JsonParsingFailure extends Failure {}
+
+class NoInternetFalure extends Failure {}
