c7bde42ca63173f72b5ac78e5fcfef14ddf1b885
aap01 <aap.pavel@gmail.com>
2022-06-17 02:48:22 +0600

    chore: update get_photo_list_usecase.dart removed unnecessary package

--- lib/feature/gallery/domain/usecase/get_photo_list_usecase.dart
+++ lib/feature/gallery/domain/usecase/get_photo_list_usecase.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:cloud_photo_gallery/core/failure/failure.dart';
 import 'package:cloud_photo_gallery/core/failure/get_photo_list_failure.dart';
 import 'package:cloud_photo_gallery/core/usecase/usecase.dart';
 import 'package:cloud_photo_gallery/feature/gallery/domain/entity/photo.dart';
@@ -8,7 +7,8 @@ import 'package:dartz/dartz.dart';
 
 import '../../../../core/usecase/params/page_listing_params.dart';
 
-class GetPhotoListUsecase implements Usecase<PageListingParams, List<Photo>, GetPhotoListFailure> {
+class GetPhotoListUsecase
+    implements Usecase<PageListingParams, List<Photo>, GetPhotoListFailure> {
   late final PhotoRepository _photoRepository;
   GetPhotoListUsecase({
     required PhotoRepository photoRepository,
