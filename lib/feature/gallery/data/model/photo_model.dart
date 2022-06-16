b2631b403fc75f60668721b7efd144d03e7df6fd
aap01 <aap.pavel@gmail.com>
2022-06-16 19:39:06 +0600

    update photo_model.dart

--- lib/feature/gallery/data/model/photo_model.dart
+++ lib/feature/gallery/data/model/photo_model.dart
@@ -1,34 +1,27 @@
 import 'package:cloud_photo_gallery/feature/gallery/data/model/links_model.dart';
-import 'package:cloud_photo_gallery/feature/gallery/data/model/model.dart';
 import 'package:cloud_photo_gallery/feature/gallery/data/model/urls_model.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 import '../../domain/entity/photo.dart';
 
-part 'photo_model.freezed.dart';
 part 'photo_model.g.dart';
 
-@freezed
-class PhotoModel with _$PhotoModel implements Model<Photo> {
-  const PhotoModel._();
-  const factory PhotoModel({
-    String? id,
-    int? width,
-    int? height,
-    String? color,
-    @JsonKey(name: 'urls') UrlsModel? urlsModel,
-    @JsonKey(name: 'links') LinksModel? linksModel,
-  }) = _PhotoModel;
+@JsonSerializable()
+class PhotoModel extends Photo {
+  @JsonKey(name: 'links')
+  LinksModel? linksModel;
+  @JsonKey(name: 'urls')
+  UrlsModel? urlsModel;
+
+  PhotoModel({
+    required super.id,
+    required super.width,
+    required super.height,
+    this.linksModel,
+    this.urlsModel,
+  });
 
   factory PhotoModel.fromJson(Map<String, dynamic> json) =>
       _$PhotoModelFromJson(json);
-
-  @override
-  Photo toEntity() => Photo(
-        id: id ?? '',
-        width: width ?? 0,
-        height: height ?? 0,
-        urls: urlsModel?.toEntity(),
-        links: linksModel?.toEntity(),
-      );
+  Map<String, dynamic> toJson() => _$PhotoModelToJson(this);
 }
