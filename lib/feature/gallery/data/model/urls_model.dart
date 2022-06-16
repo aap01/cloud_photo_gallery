356e3bec59030dd947bc2c9e200acc8c06056b92
aap01 <aap.pavel@gmail.com>
2022-06-16 19:38:53 +0600

    update urls_model.dart

--- lib/feature/gallery/data/model/urls_model.dart
+++ lib/feature/gallery/data/model/urls_model.dart
@@ -1,33 +1,22 @@
-import 'package:cloud_photo_gallery/feature/gallery/data/model/model.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 import '../../domain/entity/urls.dart';
 
-part 'urls_model.freezed.dart';
 part 'urls_model.g.dart';
 
-@freezed
-abstract class UrlsModel with _$UrlsModel implements Model<Urls> {
-  const UrlsModel._();
-  const factory UrlsModel({
-    String? raw,
-    String? full,
-    String? regular,
-    String? small,
-    String? thumb,
-    String? smallS3,
-  }) = _UrlsModel;
+@JsonSerializable()
+class UrlsModel extends Urls {
+  UrlsModel({
+    required super.raw,
+    required super.full,
+    required super.regular,
+    required super.small,
+    required super.thumb,
+    required super.smallS3,
+  });
 
   factory UrlsModel.fromJson(Map<String, dynamic> json) =>
       _$UrlsModelFromJson(json);
 
-  @override
-  Urls toEntity() => Urls(
-        raw: raw ?? '',
-        full: full ?? '',
-        regular: regular ?? '',
-        small: small ?? '',
-        thumb: thumb ?? '',
-        smallS3: smallS3 ?? '',
-      );
+  Map<String, dynamic> toJson() => _$UrlsModelToJson(this);
 }
