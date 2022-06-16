1b2b1c9f0513dd6f75687c88b4a3404c0ceb0b5d
aap01 <aap.pavel@gmail.com>
2022-06-17 02:51:04 +0600

    chore: update photolist_cubit.dart and photolist_state.dart renamed to PhotoListCubit

--- lib/feature/gallery/presentation/cubit/photolist_state.dart
+++ lib/feature/gallery/presentation/cubit/photolist_state.dart
@@ -1,9 +1,10 @@
 part of 'photolist_cubit.dart';
 
 @freezed
-class PhotolistState with _$PhotolistState {
-  const factory PhotolistState.initial() = _Initial;
-  const factory PhotolistState.loading() = _Loading;
-  const factory PhotolistState.loaded({required List<Photo> photos}) = _Loaded;
-  const factory PhotolistState.failed({required GetPhotoListFailure failure}) = _Failure;
+class PhotoListState with _$PhotoListState {
+  const factory PhotoListState.initial() = _Initial;
+  const factory PhotoListState.loading() = _Loading;
+  const factory PhotoListState.loaded({required List<Photo> photos}) = _Loaded;
+  const factory PhotoListState.failed({required GetPhotoListFailure failure}) =
+      _Failure;
 }
