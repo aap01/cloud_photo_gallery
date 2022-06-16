892d2363cf8048c34e51f91295592e1e4bc325da
aap01 <aap.pavel@gmail.com>
2022-06-16 11:22:21 +0600

    update photo.dart

--- lib/feature/gallery/domain/entity/photo.dart
+++ lib/feature/gallery/domain/entity/photo.dart
@@ -4,25 +4,15 @@ import 'package:cloud_photo_gallery/feature/gallery/domain/entity/urls.dart';
 
 class Photo {
   final String id;
-  final DateTime createdAt;
-  final DateTime updatedAt;
-  final DateTime promotedAt;
   final int width;
   final int height;
-  final String color;
-  final String blurHash;
-  final Urls urls;
-  final Links links;
+  final Urls? urls;
+  final Links? links;
   Photo({
     required this.id,
-    required this.createdAt,
-    required this.updatedAt,
-    required this.promotedAt,
     required this.width,
     required this.height,
-    required this.color,
-    required this.blurHash,
-    required this.urls,
-    required this.links,
+    this.urls,
+    this.links,
   });
 }
