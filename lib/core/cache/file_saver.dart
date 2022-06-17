d9410d18e677e9d2ec3b2c3495009628ac9285f1
aap01 <aap.pavel@gmail.com>
2022-06-18 04:32:17 +0600

    feat: create file_saver.dart

---
+++ lib/core/cache/file_saver.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+
+class FileHelper {
+  static Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  static Future<File> download(String filename, String filePath) async {
+    final file = File(filePath);
+    final bytes = await file.readAsBytes();
+    final newFile = File('${await _localPath}/$filename');
+    return await newFile.writeAsBytes(bytes);
+  }
+
+  static Future<OpenResult> open(String path) {
+    return OpenFile.open(path);
+  }
+}
