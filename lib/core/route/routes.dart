5fc483b03c18817263dff4f661329f17cb35c12d
aap01 <aap.pavel@gmail.com>
2022-06-17 23:32:48 +0600

    update routes.dart

--- lib/core/route/routes.dart
+++ lib/core/route/routes.dart
@@ -1,15 +1,17 @@
 import 'package:cloud_photo_gallery/core/constant/title_constants.dart';
-import 'package:cloud_photo_gallery/feature/gallery/presentation/screen/home_page.dart';
+import 'package:cloud_photo_gallery/feature/gallery/presentation/screen/gallery_screen.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
   static const home = '/';
+  static const photoViewScreen = '/photo_view_screen';
+
   Routes._();
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case home:
         return _getRoute(
-          child: const HomePage(),
+          child: const GalleryScreen(),
           settings: settings,
         );
       default:
