5f554e1e0af9729c6e81f72dd1e5adb1259691e7
aap01 <aap.pavel@gmail.com>
2022-06-18 04:32:29 +0600

    update service_locator.dart

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_photo_gallery/core/api/api_impl.dart';
+import 'package:cloud_photo_gallery/core/cache/cache_manager.dart';
 import 'package:cloud_photo_gallery/core/network/network_connection_checker_impl.dart';
 import 'package:cloud_photo_gallery/feature/gallery/data/data_source/remote/photo_remote_source_impl.dart';
 import 'package:cloud_photo_gallery/feature/gallery/data/repository/photo_repository_impl.dart';
 import 'package:cloud_photo_gallery/feature/gallery/domain/usecase/get_photo_list_usecase.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -26,10 +28,15 @@ class ServiceLocator {
       () => ApiImpl(dio: sl()),
     );
     sl.registerLazySingleton(
-      () => Dio(),
-    );
-    sl.registerLazySingleton(
       () => NetworkConnectionCheckerImpl(),
     );
+
+    sl.registerLazySingleton(() => AppCacheManager(baseCacheManager: sl()));
+
+    // 3rd party
+    sl.registerLazySingleton(
+      () => Dio(),
+    );
+    sl.registerLazySingleton<BaseCacheManager>(() => DefaultCacheManager());
   }
 }
