d871c97ec536a44a2ffa1cdccbcff2d92d254f51
aap01 <aap.pavel@gmail.com>
2022-06-18 01:26:31 +0600

    feat: create theme_screen.dart

---
+++ lib/core/screen/theme_screen.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class ThemeScreen extends StatelessWidget {
+  const ThemeScreen({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: child));
+  }
+}
