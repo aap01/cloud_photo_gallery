51c8a3b7f6ef8cbfa9c38c93d37bab84f11123ed
aap01 <aap.pavel@gmail.com>
2022-06-17 23:29:33 +0600

    feat: create grid_pattern_constants.dart

---
+++ lib/feature/gallery/presentation/widget/grid_pattern_constants.dart
@@ -0,0 +1,86 @@
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class GridPatternConstants {
+  const GridPatternConstants._();
+  static const galleryPattern = [
+    // group little
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group humpty
+    QuiltedGridTile(2, 2),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 2),
+    // group little
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group twinkle
+    QuiltedGridTile(3, 3),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group little
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group center
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(2, 2),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group little
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group twinkle inverted
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(3, 3),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group little
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group humpty inverted
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(2, 2),
+    QuiltedGridTile(1, 2),
+    // group little
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group flattened
+    QuiltedGridTile(2, 3),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group little
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group humpty inverted
+    QuiltedGridTile(1, 2),
+    QuiltedGridTile(2, 2),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group little
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(1, 1),
+    // group flattened inverted
+    QuiltedGridTile(1, 1),
+    QuiltedGridTile(2, 3),
+    QuiltedGridTile(1, 1),
+  ];
+}
