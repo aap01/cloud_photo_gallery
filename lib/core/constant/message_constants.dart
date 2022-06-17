189c15f9ef97a83255095696e5a9cf579b710cc5
aap01 <aap.pavel@gmail.com>
2022-06-17 18:17:03 +0600

    update message_constants.dart

--- lib/core/constant/message_constants.dart
+++ lib/core/constant/message_constants.dart
@@ -2,7 +2,9 @@ class MessageConstants {
   static const unHandledException = 'Unhandled error';
   static const defaultServerFailureMessage = 'Failed to connect to server';
 
-  static const noInternet = 'No Internet connection';
+  static const noInternet = 'No internet connection';
+
+  static const internalError = 'Internal error';
 
   MessageConstants._();
 }
