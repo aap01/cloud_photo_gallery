eb988467a10ea59ab22dadc269a3fcbe99f7711f
aap01 <aap.pavel@gmail.com>
2022-06-18 04:32:25 +0600

    update title_constants.dart

--- lib/core/constant/title_constants.dart
+++ lib/core/constant/title_constants.dart
@@ -3,6 +3,12 @@ class TitleConstants {
 
   static const tryAgain = 'Try Again';
 
+  static const sharedImage = 'Shared';
+
+  static const imageDownloaded = 'Downloaded';
+
+  static const open = 'Open';
+
   TitleConstants._();
   static const appTitle = 'Cloud Photo Gallery';
 }
