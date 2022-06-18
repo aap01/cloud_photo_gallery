6f838af4436b186c47067519e736111313d845f9
aap01 <aap.pavel@gmail.com>
2022-06-18 19:33:15 +0600

    update file_saver.dart

--- lib/core/cache/file_saver.dart
+++ lib/core/cache/file_saver.dart
@@ -4,7 +4,7 @@ import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 
 class FileHelper {
-  static Future<String> get _localPath async {
+  static Future<String> get documentsDirectoryPath async {
     final directory = await getApplicationDocumentsDirectory();
     return directory.path;
   }
@@ -12,7 +12,7 @@ class FileHelper {
   static Future<File> download(String filename, String filePath) async {
     final file = File(filePath);
     final bytes = await file.readAsBytes();
-    final newFile = File('${await _localPath}/$filename');
+    final newFile = File('${await documentsDirectoryPath}/$filename');
     return await newFile.writeAsBytes(bytes);
   }
 
