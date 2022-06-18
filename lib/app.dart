32d5f7499f0f95ebf5ed7e543e1a0371c4bcdc73
aap01 <aap.pavel@gmail.com>
2022-06-18 19:38:53 +0600

    update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,16 @@
 import 'package:cloud_photo_gallery/core/constant/build_constants.dart';
+import 'package:cloud_photo_gallery/core/di/service_locator.dart';
 import 'package:cloud_photo_gallery/core/route/routes.dart';
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,5 +21,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: BuildConstants.debug,
       onGenerateRoute: Routes.onGenerateRoute,
     );
+  }
+
+  @override
+  void dispose() async {
+    await ServiceLocator.closeServices();
+    super.dispose();
   }
 }
