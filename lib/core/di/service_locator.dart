cbf2de6410a6f91a22ea8c5cbf7e57e2eb9f3e48
aap01 <aap.pavel@gmail.com>
2022-06-18 19:33:42 +0600

    update service_locator.dart

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,11 +1,14 @@
 import 'package:cloud_photo_gallery/core/api/api_impl.dart';
 import 'package:cloud_photo_gallery/core/cache/cache_manager.dart';
+import 'package:cloud_photo_gallery/core/cache/file_saver.dart';
 import 'package:cloud_photo_gallery/core/network/netowork_connection_checker.dart';
 import 'package:cloud_photo_gallery/core/network/network_connection_checker_impl.dart';
 import 'package:cloud_photo_gallery/feature/gallery/data/data_source/remote/photo_remote_source_impl.dart';
 import 'package:cloud_photo_gallery/feature/gallery/data/repository/photo_repository_impl.dart';
 import 'package:cloud_photo_gallery/feature/gallery/domain/usecase/get_photo_list_usecase.dart';
 import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:get_it/get_it.dart';
 
@@ -29,7 +32,10 @@ class ServiceLocator {
       () => PhotoRemoteDataSourceImpl(api: sl()),
     );
     sl.registerLazySingleton<Api>(
-      () => ApiImpl(dio: sl()),
+      () => ApiImpl(
+        dio: sl(),
+        cacheStore: sl(),
+      ),
     );
     sl.registerLazySingleton<NetworkConnectionChecker>(
       () => NetworkConnectionCheckerImpl(),
@@ -39,6 +45,8 @@ class ServiceLocator {
       () => AppCacheManagerImpl(baseCacheManager: sl()),
     );
 
+    final appDirectory = await FileHelper.documentsDirectoryPath;
+    
     // 3rd party
     sl.registerLazySingleton(
       () => Dio(),
@@ -46,5 +54,12 @@ class ServiceLocator {
     sl.registerLazySingleton<BaseCacheManager>(
       () => DefaultCacheManager(),
     );
+    sl.registerLazySingleton<CacheStore>(() => HiveCacheStore(appDirectory));
+  }
+
+  static Future<void> closeServices() async {
+    sl<Dio>().close();
+    await sl<CacheStore>().close();
+    await sl<BaseCacheManager>().dispose();
   }
 }
