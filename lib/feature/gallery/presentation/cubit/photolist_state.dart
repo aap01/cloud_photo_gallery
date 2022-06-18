e61c81fbf095b6d383dc118278150442b521c444
aap01 <aap.pavel@gmail.com>
2022-06-19 05:27:47 +0600

    update photolist_state.dart

--- lib/feature/gallery/presentation/cubit/photolist_state.dart
+++ lib/feature/gallery/presentation/cubit/photolist_state.dart
@@ -5,6 +5,7 @@ class PhotoListState with _$PhotoListState {
   const factory PhotoListState.initial() = _Initial;
   const factory PhotoListState.loading() = _Loading;
   const factory PhotoListState.loaded({required List<Photo> photos}) = _Loaded;
-  const factory PhotoListState.failed({required GetPhotoListFailure failure}) =
-      _Failure;
+  const factory PhotoListState.noInternet() = _NoInternet;
+  const factory PhotoListState.otherError({required String message}) =
+      _OtherError;
 }
