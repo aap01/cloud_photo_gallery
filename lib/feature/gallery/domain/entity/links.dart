3a029194be4c2ea4073be745f16a6b3c3abeb6e2
aap01 <aap.pavel@gmail.com>
2022-06-20 02:13:37 +0600

    update links.dart

--- lib/feature/gallery/domain/entity/links.dart
+++ lib/feature/gallery/domain/entity/links.dart
@@ -1,13 +1,18 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-class Links {
+import 'package:equatable/equatable.dart';
+
+class Links extends Equatable {
   final String self;
   final String html;
   final String download;
   final String downloadLocation;
-  Links({
+  const Links({
     required this.self,
     required this.html,
     required this.download,
     required this.downloadLocation,
   });
+
+  @override
+  List<Object?> get props => [self, html, download, downloadLocation];
 }
