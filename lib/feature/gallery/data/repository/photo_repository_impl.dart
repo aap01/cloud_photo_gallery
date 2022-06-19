33a1a0c5f69d54a9f962facf737672ca33cc4949
aap01 <aap.pavel@gmail.com>
2022-06-20 02:15:26 +0600

    update photo_repository_impl.dart

--- lib/feature/gallery/data/repository/photo_repository_impl.dart
+++ lib/feature/gallery/data/repository/photo_repository_impl.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cloud_photo_gallery/core/constant/message_constants.dart';
 import 'package:cloud_photo_gallery/core/exception/server_exception.dart';
 import 'package:cloud_photo_gallery/core/failure/get_photo_list_failure.dart';
 import 'package:cloud_photo_gallery/core/network/netowork_connection_checker.dart';
 import 'package:cloud_photo_gallery/feature/gallery/data/data_source/remote/photo_remote_data_source.dart';
-import 'package:cloud_photo_gallery/feature/gallery/data/model/photo_list_model.dart';
 import 'package:cloud_photo_gallery/feature/gallery/domain/entity/photo.dart';
 import 'package:cloud_photo_gallery/feature/gallery/domain/repository/photo_repository.dart';
 import 'package:dartz/dartz.dart';
@@ -26,22 +26,10 @@ class PhotoRepositoryImpl implements PhotoRepository {
   }) async {
     try {
       final response = await _photoRemoteDataSource.fetchPhotos(
-        page: page,
         perPage: perPage,
+        page: page,
       );
-      return response.when(
-        error: (value) {
-          return Left(
-            GetPhotoListFailure.server(
-              message: value.errors.first,
-            ),
-          );
-        },
-        success: (value) {
-          final photoListModel = PhotoListModel.fromJson({'photos': value});
-          return Right(photoListModel.photos);
-        },
-      );
+      return Right(response);
     } on ServerException catch (e) {
       final isConnected = await _networkConnectionChecker.isConnected();
       if (!isConnected) {
@@ -51,6 +39,10 @@ class PhotoRepositoryImpl implements PhotoRepository {
       }
       return Left(
         GetPhotoListFailure.server(message: e.message),
+      );
+    } on Exception {
+      return const Left(
+        GetPhotoListFailure.server(message: MessageConstants.internalError),
       );
     }
   }
