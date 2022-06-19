a5205a884860b8d788644c63a1419410da9c19ee
aap01 <aap.pavel@gmail.com>
2022-06-20 02:15:04 +0600

    update photo_remote_data_source.dart

--- lib/feature/gallery/data/data_source/remote/photo_remote_data_source.dart
+++ lib/feature/gallery/data/data_source/remote/photo_remote_data_source.dart
@@ -1,9 +1,15 @@
-import 'package:cloud_photo_gallery/core/api/api_response.dart';
+import 'package:cloud_photo_gallery/core/api/base_api.dart';
+import 'package:cloud_photo_gallery/feature/gallery/data/model/page_listing_model.dart';
 
-abstract class PhotoRemoteDataSource {
+import '../../model/photo_model.dart';
+
+abstract class PhotoRemoteDataSource extends BaseApi {
+
+  String get photoListEndpoint;
+
   //Throws Server Exception
-  Future<ApiResponse> fetchPhotos({
+  Future<List<PhotoModel>> fetchPhotos({
+     required int perPage,
     required int page,
-    required int perPage,
   });
 }
