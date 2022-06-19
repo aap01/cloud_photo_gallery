b6392a0b12fb238d5143293ad9e6e340bff8f549
aap01 <aap.pavel@gmail.com>
2022-06-20 02:13:44 +0600

    update photo.dart

--- lib/feature/gallery/domain/entity/photo.dart
+++ lib/feature/gallery/domain/entity/photo.dart
@@ -1,18 +1,27 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:cloud_photo_gallery/feature/gallery/domain/entity/links.dart';
 import 'package:cloud_photo_gallery/feature/gallery/domain/entity/urls.dart';
+import 'package:equatable/equatable.dart';
 
-class Photo {
+class Photo extends Equatable {
   final String id;
   final int width;
   final int height;
-  final Urls? urls;
-  final Links? links;
-  Photo({
+  final Urls urls;
+  final Links links;
+  const Photo({
     required this.id,
     required this.width,
     required this.height,
-    this.urls,
-    this.links,
+    required this.urls,
+    required this.links,
   });
+  @override
+  List<Object?> get props => [
+        id,
+        width,
+        height,
+        urls,
+        links,
+      ];
 }
