aa31d9c961f654c22bbed6b3952909ad35664768
aap01 <aap.pavel@gmail.com>
2022-07-09 02:56:52 +0600

    update photo_model.dart PhotoModel does not inherit Photo

--- lib/feature/gallery/data/model/photo_model.dart
+++ lib/feature/gallery/data/model/photo_model.dart
@@ -2,27 +2,25 @@ import 'package:cloud_photo_gallery/feature/gallery/data/model/links_model.dart'
 import 'package:cloud_photo_gallery/feature/gallery/data/model/urls_model.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-import '../../domain/entity/photo.dart';
-
 part 'photo_model.g.dart';
 
 @JsonSerializable()
-class PhotoModel extends Photo {
+class PhotoModel {
+  final String id;
+  final int width;
+  final int height;
   @JsonKey(name: 'links')
   final LinksModel linksModel;
   @JsonKey(name: 'urls')
   final UrlsModel urlsModel;
 
   const PhotoModel({
-    required super.id,
-    required super.width,
-    required super.height,
+    required this.id,
+    required this.width,
+    required this.height,
     required this.linksModel,
     required this.urlsModel,
-  }) : super(
-          links: linksModel,
-          urls: urlsModel,
-        );
+  });
 
   factory PhotoModel.fromJson(Map<String, dynamic> json) =>
       _$PhotoModelFromJson(json);
