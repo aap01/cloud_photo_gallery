2acb2dd8087e5cd7221b05ae1c5bd0153785c439
aap01 <aap.pavel@gmail.com>
2022-07-09 02:58:42 +0600

    update links_model_test.dart LinksModel is not a subtype of Links

--- test/feature/gallery/data/model/links_model_test.dart
+++ test/feature/gallery/data/model/links_model_test.dart
@@ -1,16 +1,9 @@
 import 'package:cloud_photo_gallery/feature/gallery/data/model/links_model.dart';
-import 'package:cloud_photo_gallery/feature/gallery/domain/entity/links.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../../fixture/links_map.dart';
 
 void main() async {
-  test(
-    'Links model is a subtype of Links',
-    () async {
-      expect(linksModel, isA<Links>());
-    },
-  );
   test(
     'Links.fromJson()',
     () async {
