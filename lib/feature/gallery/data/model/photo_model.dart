d1d2599d18733c528ff7420baacb410eb61add25
aap01 <aap.pavel@gmail.com>
2022-07-09 03:12:05 +0600

    update photo_model.dart Inherits Equatable

--- lib/feature/gallery/data/model/photo_model.dart
+++ lib/feature/gallery/data/model/photo_model.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_photo_gallery/feature/gallery/data/model/links_model.dart';
 import 'package:cloud_photo_gallery/feature/gallery/data/model/urls_model.dart';
+import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'photo_model.g.dart';
 
 @JsonSerializable()
-class PhotoModel {
+class PhotoModel extends Equatable {
   final String id;
   final int width;
   final int height;
@@ -25,4 +26,13 @@ class PhotoModel {
   factory PhotoModel.fromJson(Map<String, dynamic> json) =>
       _$PhotoModelFromJson(json);
   Map<String, dynamic> toJson() => _$PhotoModelToJson(this);
+
+  @override
+  List<Object?> get props => [
+        id,
+        width,
+        height,
+        linksModel,
+        urlsModel,
+      ];
 }
