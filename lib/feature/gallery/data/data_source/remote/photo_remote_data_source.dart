2f301868d1ecec145e73814b5e775458750474b7
aap01 <aap.pavel@gmail.com>
2022-07-09 02:52:43 +0600

    update photo_remote_data_source.dart Removed unnecessary import

--- lib/feature/gallery/data/data_source/remote/photo_remote_data_source.dart
+++ lib/feature/gallery/data/data_source/remote/photo_remote_data_source.dart
@@ -1,15 +1,13 @@
 import 'package:cloud_photo_gallery/core/api/base_api.dart';
-import 'package:cloud_photo_gallery/feature/gallery/data/model/page_listing_model.dart';
 
 import '../../model/photo_model.dart';
 
 abstract class PhotoRemoteDataSource extends BaseApi {
-
   String get photoListEndpoint;
 
   //Throws Server Exception
   Future<List<PhotoModel>> fetchPhotos({
-     required int perPage,
+    required int perPage,
     required int page,
   });
 }
