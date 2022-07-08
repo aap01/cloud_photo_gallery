dfff290f9187e60b54ed3d446791c3b67eb564ce
aap01 <aap.pavel@gmail.com>
2022-07-09 02:59:34 +0600

    update urls_model_test.dart UrlsModel is not a subtype of Urls

--- test/feature/gallery/data/model/urls_model_test.dart
+++ test/feature/gallery/data/model/urls_model_test.dart
@@ -1,19 +1,10 @@
 import 'package:cloud_photo_gallery/feature/gallery/data/model/urls_model.dart';
-import 'package:cloud_photo_gallery/feature/gallery/domain/entity/urls.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../../../fixture/urls_map.dart';
 
 void main() {
   final urlsModel = mappedUrlsModel;
-  test(
-    'UrlsModel is a subtype of Urls',
-    () => expect(
-      urlsModel,
-      isA<Urls>(),
-    ),
-  );
-
   test(
     'UrlsModel.fromJson()',
     () async {
