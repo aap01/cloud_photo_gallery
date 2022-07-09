cac2c6d964b33d8007bcec1093bf0a8c99cfc348
aap01 <aap.pavel@gmail.com>
2022-07-09 06:07:51 +0600

    update photo_repository_impl_test.dart Test passes for success

--- test/feature/gallery/data/repository/photo_repository_impl_test.dart
+++ test/feature/gallery/data/repository/photo_repository_impl_test.dart
@@ -1,44 +1,83 @@
 import 'package:cloud_photo_gallery/core/constant/message_constants.dart';
-import 'package:cloud_photo_gallery/core/exception/server_exception.dart';
 import 'package:cloud_photo_gallery/core/failure/get_photo_list_failure.dart';
 import 'package:cloud_photo_gallery/core/network/netowork_connection_checker.dart';
 import 'package:cloud_photo_gallery/feature/gallery/data/data_source/remote/photo_remote_data_source.dart';
+import 'package:cloud_photo_gallery/feature/gallery/data/model/links_model.dart';
 import 'package:cloud_photo_gallery/feature/gallery/data/model/photo_model.dart';
+import 'package:cloud_photo_gallery/feature/gallery/data/model/urls_model.dart';
 import 'package:cloud_photo_gallery/feature/gallery/data/repository/photo_repository_impl.dart';
+import 'package:cloud_photo_gallery/feature/gallery/domain/entity/links.dart';
+import 'package:cloud_photo_gallery/feature/gallery/domain/entity/photo.dart';
+import 'package:cloud_photo_gallery/feature/gallery/domain/entity/urls.dart';
+import 'package:cloud_photo_gallery/feature/gallery/domain/mapper/photo_list_mapper.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import '../../../../fixture/photo_map.dart';
 import 'photo_repository_impl_test.mocks.dart';
 
 @GenerateMocks([
   PhotoRemoteDataSource,
   NetworkConnectionChecker,
+  PhotoListMapper,
 ])
 void main() async {
   final mockPhotoRemoteDataSourece = MockPhotoRemoteDataSource();
   final mockNetworkConnectionChecker = MockNetworkConnectionChecker();
+  final mockMapper = MockPhotoListMapper();
   final photoRepository = PhotoRepositoryImpl(
     photoRemoteDataSource: mockPhotoRemoteDataSourece,
     networkConnectionChecker: mockNetworkConnectionChecker,
+    mapper: mockMapper,
   );
+  const linksModel = LinksModel(
+    self: 's',
+    html: 'h',
+    download: 'd',
+    downloadLocation: 'd',
+  );
+  const links = Links(
+    self: 's',
+    html: 'h',
+    download: 'd',
+    downloadLocation: 'd',
+  );
+  const urlsModel = UrlsModel(
+    raw: 'r',
+    full: 'f',
+    regular: 'r',
+    small: 's',
+    thumb: 't',
+    smallS3: 's',
+  );
+  const urls = Urls(
+    raw: 'r',
+    full: 'f',
+    regular: 'r',
+    small: 's',
+    thumb: 't',
+    smallS3: 's',
+  );
+  const photoModel = PhotoModel(
+    id: '1',
+    width: 1,
+    height: 1,
+    linksModel: linksModel,
+    urlsModel: urlsModel,
+  );
+  const photo = Photo(
+    id: '1',
+    width: 1,
+    height: 1,
+    links: links,
+    urls: urls,
+  );
+  const photoModelList = [photoModel];
+  const photoList = [photo];
 
   const int perPage = 10;
   const int page = 1;
-  final response = [await getPhotoMap()];
-  final List<PhotoModel> photos =
-      response.map((e) => PhotoModel.fromJson(e)).toList();
-  const serverException =
-      ServerException(message: MessageConstants.internalError);
-
-  // final errorString = await FixtureReader.read('test/fixture/error.json');
-  // final Map<String, dynamic> errorJson = json.decode(errorString);
-  // final errorModel = ErrorModel.fromJson(errorJson);
-  // final apiErrorResponse = ApiResponse.error(error: errorModel);
-  // final getPhotoListFailure =
-  //     GetPhotoListFailure.server(message: errorModel.errors.first);
   test(
     'should return Right(photoListModel.photos)',
     () async {
@@ -47,7 +86,14 @@ void main() async {
           perPage: perPage,
           page: page,
         ),
-      ).thenAnswer((_) async => Future<List<PhotoModel>>.value(photos));
+      ).thenAnswer(
+        (_) async => Future<List<PhotoModel>>.value(photoModelList),
+      );
+      when(
+        mockMapper.map(
+          photoModelList,
+        ),
+      ).thenReturn(photoList);
 
       final result = await photoRepository.getPhotos(
         perPage: perPage,
@@ -60,7 +106,7 @@ void main() async {
         ),
       );
       verifyNoMoreInteractions(mockPhotoRemoteDataSourece);
-      expect(result, equals(Right(photos)));
+      expect(result, equals(const Right(photoList)));
     },
   );
   test(
